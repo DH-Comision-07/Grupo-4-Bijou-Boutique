7e22a8096abb865369771cf6e1ab5440f89a39ca
Abril-Gil <abrilgilprogramacion@gmail.com>
2024-02-28 01:56:50 -0300

    Configuracion del productDetail

--- app.js
+++ app.js
@@ -3,12 +3,16 @@ const path = require("path");
 const app = express();
 const port = process.env.PORT || 3030;
 
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "./views/home.html"));
+});
+
+app.get("/productDetail", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "./views/productDetail.html"));
+});
+
 app.listen(port, () =>
   console.log(`Proyecto corriendo exitosamente en puerto ${port}`)
 );
-
-app.get("/", (req, res) => {
-  res.sendFile(path.resolve("./views/home.html"));
-});
-
-app.use(express.static("public"));
