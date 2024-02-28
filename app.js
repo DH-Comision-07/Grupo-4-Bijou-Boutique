aa1820084faabd958c492746040db59ff4e3fd1b
DPalermo26 <palermodanilo01@gmail.com>
2024-02-28 01:36:39 -0300

    Commit Home y configuracion general

--- app.js
+++ app.js
@@ -1,22 +1,14 @@
-const express = require('express');
-const app = express();
-
+const express = require("express");
 const path = require("path");
-const publicPath = path.resolve(__dirname, "public");
-app.use(express.static(publicPath));
+const app = express();
+const port = process.env.PORT || 3030;
 
-const port = process.env.PORT || 3001;
-app.listen(port,() => console.log("Servidor corriendo en el puerto ${port}"));
-
+app.listen(port, () =>
+  console.log(`Proyecto corriendo exitosamente en puerto ${port}`)
+);
 
 app.get("/", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "views/index.html"));
+  res.sendFile(path.resolve("./views/home.html"));
 });
 
-app.get("/register", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "views/register.html"));
-});
-
-app.get("/login", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "views/login.html"));
-});
+app.use(express.static("public"));
