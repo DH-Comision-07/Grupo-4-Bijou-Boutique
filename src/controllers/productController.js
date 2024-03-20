ff05cc2b2813d0fffea5b8c86424d7a843ddaef7
johannes003 <johannesandres03@gmail.com>
2024-03-20 08:33:51 -0500

    CRUD de products

--- src/controllers/productController.js
+++ src/controllers/productController.js
@@ -1,3 +1,5 @@
+const { Router } = require("express");
+
 const productController = {
   productDetail: (req, res) => {
     return res.render("productDetail");
@@ -9,5 +11,55 @@ const productController = {
     return res.render("formulario");
   },
 };
+
+
+app.get('/products', (req, res) => {
+    fs.readFile(productsFile, 'utf8', (err, data) => {
+        if (err) {
+            res.status(500).send('Error al leer los productos.');
+            return;
+        }
+    });
+});
+
+// Formulario de Creación de Productos
+app.get('/products/create', (req, res) => {
+    
+});
+
+app.get('/products/:id', (req, res) => {
+    const productId = req.params.id;
+    fs.readFile(productsFile, 'utf8', (err, data) => {
+        if (err) {
+            res.status(500).send('Error al leer los productos.');
+            return;
+        }
+        const products = JSON.parse(data);
+        const product = products.find(prod => prod.id === parseInt(productId));
+        if (!product) {
+            res.status(404).send('Producto no encontrado.');
+            return;
+        }
+        res.json(product);
+    });
+});
+
+// Acción de Creación
+app.post('/products', (req, res) => {
+});
+
+// Formulario de Edición de Productos
+app.get('/products/:id/edit', (req, res) => {
+});
+
+// Acción de Edición
+app.put('/products/:id', (req, res) => {
+});
+
+// Acción de Borrado
+app.delete('/products/:id', (req, res) => {
+});
+
+
 
 module.exports = productController;
