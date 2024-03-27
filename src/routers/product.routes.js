dffac50eb65efb474813026b9002b2dea006a7d4
DPalermo26 <palermodanilo01@gmail.com>
2024-03-27 00:35:23 -0300

    Fixed routes

--- src/routers/product.routes.js
+++ src/routers/product.routes.js
@@ -1,14 +1,13 @@
 const express = require("express");
 const router = express.Router();
 const productController = require("../controllers/productController");
-const indexController = require("../controllers/indexController");
 
-router.get("/", indexController.index);
 router.get("/cart", productController.cart);
 router.get("/productCard", productController.productCard);
 router.get("/productDetail", productController.productDetail);
 router.get("/formulary", productController.formulary);
 
 //  GET ONE PRODUCT
+// router.get("/:id/", productController.detail);
 
 module.exports = router;
