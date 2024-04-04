d57c0d49a63a83744dcb80c5689659ffe16bcbd7
johannes003 <johannesandres03@gmail.com>
2024-04-03 21:55:13 -0500

    crud js users

--- src/models/userService.js
+++ src/models/userService.js
@@ -2,6 +2,16 @@ const fs = require("fs");
 const path = require("path");
 const users = require("../models/users.json");
 
+const userService = {
+  users: users,
+  
+  save:function(user){
+    this.users.push(user);
+    fs.writeFileSync(path.resolve(__dirname,'../models/users.json'),JSON.stringify(users));
+},
+}
 
+
+module.exports = userService;
 
 
