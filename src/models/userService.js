197b0dadb248d428a0c16913c6259de16c368f22
johannes003 <johannesandres03@gmail.com>
2024-04-03 22:34:20 -0500

    users

--- src/models/userService.js
+++ src/models/userService.js
@@ -4,11 +4,14 @@ const users = require("../models/users.json");
 
 const userService = {
   users: users,
+  getAll: function () {
+    return this.users;
+  },
+  getOne: function (id) {
+    return this.users.find((user) => user.id == id);
+  },
   save:function(user){
     this.users.push(user);
     fs.writeFileSync(path.resolve(__dirname,'../models/users.json'),JSON.stringify(users));
-},
+ },
 }
-module.exports = userService;
-
-
