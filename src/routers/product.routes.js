0b9b028ef77a28b5a5b2b311b374e673e7815f0c
johannes003 <johannesandres03@gmail.com>
2024-04-23 11:13:10 -0500

    img

--- src/routers/product.routes.js
+++ src/routers/product.routes.js
@@ -1,6 +1,18 @@
 const express = require("express");
 const router = express.Router();
+const multer = require('multer');
+const path = require('path');
 const productController = require("../controllers/productController");
+const storage = multer.diskStorage({
+    destination: (req, file,cb) => {
+        cb(null,'./public/images/products')
+    },
+    filename: (req,file,cb) => {
+        const newFileName = 'products-' + Date.now() + path.extname(file.originalname);
+        cb(null,newFileName)
+    }
+});  
+const upload = multer({storage:storage});
 
 router.get("/cart", productController.cart);
 
@@ -9,7 +21,9 @@ router.get("/products", productController.products);
 router.get("/products/:id/", productController.detail);
 
 router.get("/create", productController.formulary);
+
 router.post('/',upload.single('imagen'),productController.store);
+
 router.get("/productCard", productController.productCard);
 
 router.get("/products/:id/edit", productController.editForm);
