b4dfaddc4165c75b91c21be57b5ca35fdc185d0d
Nicolas Gruger <nicolas.gruger@hotmail.com>
2024-04-30 18:41:00 -0300

    Implementación de middlewares

--- src/routers/product.routes.js
+++ src/routers/product.routes.js
@@ -2,18 +2,21 @@ const express = require("express");
 const router = express.Router();
 const productController = require("../controllers/productController");
 const upload = require("../middlewares/productMiddleware");
+const authMiddleware = require("../middlewares/authMiddleware");
+const guestMiddleware = require("../middlewares/guestMiddleware");
 
+
+// Rutas accesibles solo sin loguear
 router.get("/cart", productController.cart);
 router.get("/", productController.products);
-router.get("/create", productController.formulary);
+router.get("/create", authMiddleware, productController.formulary);
 router.post("/", upload.single("image"), productController.store);
-router.get("/:id/edit", productController.editForm);
-router.put("/:id", productController.update);
+router.get("/:id/edit", authMiddleware, productController.editForm);
+router.put("/:id", authMiddleware, productController.update);
+router.get("/productCard", authMiddleware, productController.productCard);
+router.delete("/:id", authMiddleware, productController.destroy);
+router.get("/:id", guestMiddleware, productController.detail);
 
-router.post("/", upload.single("imagen"), productController.store);
 
-router.get("/productCard", productController.productCard);
-router.delete("/:id", productController.destroy);
-router.get("/:id", productController.detail);
 
 module.exports = router;
--- src/routers/user.routes.js
+++ src/routers/user.routes.js
@@ -5,7 +5,7 @@ const userController = require("../controllers/userController");
 const upload = require("../middlewares/productMiddleware");
 const guestMiddleware = require("../middlewares/guestMiddleware");
 
-router.get("/login", userController.login);
+router.get("/login", guestMiddleware, userController.login);
 router.get("/success", userController.success);
 router.post(
   "/login",
@@ -24,9 +24,12 @@ router.get("/check", function (req, res) {
     res.send("El usuario logueado es " + req.session.usuarioLogueado.email);
   }
 });
-router.get("/register", userController.register);
-router.get("/contactUs", userController.contact);
-router.get("/create/", userController.create);
-router.post("/", upload.single("users-image"), userController.store);
+
+// Rutas accesibles solo sin loguear
+router.post('/', upload.single('users-image'), userController.store);
+router.get("/register", guestMiddleware, userController.register);
+
+router.get("/contact_us", userController.contact)
+router.get('/create/', userController.create); 
 
 module.exports = router;
