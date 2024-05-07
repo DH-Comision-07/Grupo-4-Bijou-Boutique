b52bc31261c71baabff83bb2bc6eaa48582a2dd2
Nicolas Gruger <nicolas.gruger@hotmail.com>
2024-05-07 12:05:20 -0300

    actualización database

--- database/config/config.js
+++ database/config/config.js
@@ -1,9 +1,11 @@
+require("dotenv").config();
+
 module.exports = {
   development: {
-    username: "root",
-    password: null,
-    database: "database_development",
-    host: "127.0.0.1",
+    username: process.env.DV_USERS,
+    password: process.env.DV_PASSWORD,
+    database: process.env.DV_NAME,
+    host: process.env.DV_PORT,
     dialect: "mysql",
   },
   test: {
