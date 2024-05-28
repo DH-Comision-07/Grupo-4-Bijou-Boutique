47bc432808caec60cdadf00259336b93a17e5d18
DPalermo26 <palermodanilo01@gmail.com>
2024-05-28 01:44:20 -0300

    Update y create

--- src/controllers/productController.js
+++ src/controllers/productController.js
@@ -18,27 +18,52 @@ const productController = {
       res.render("productCard", { products: products });
     });
   },
+  create: (req, res) => {
+    db.Product.create({
+      name: req.body.name,
+      description: req.body.description,
+      color: req.body.color,
+      price: req.body.price,
+      image: req.body.image,
+    })
+      .then(() => {
+        res.redirect("/products");
+      })
+      .catch((err) => {
+        res.status(500).send({ error: err.message });
+      });
+  },
   detail: (req, res) => {
     res.render("productDetail", {
       products: productService.getOne(req.params.id),
     });
   },
-  editForm: (req, res) => {
-    res.render("edit-form", {
-      products: productService.getOne(req.params.id),
+  edit: (req, res) => {
+    db.Product.findByPk(req.params.id).then(function (products) {
+      res.render("edit-form", { products: products });
     });
   },
   update: (req, res) => {
-    productService.editProduct(req.params.id, req.body);
-    const productToUpdate = productService.getOne(req.params.id);
-    const updatedProduct = {
+    const updatedData = {
       name: req.body.name,
       description: req.body.description,
       color: req.body.color,
       price: req.body.price,
-      image: productToUpdate.image,
     };
-    res.redirect("/products/productCard");
+
+    if (req.file) {
+      updatedData.image = req.file.filename;
+    }
+
+    db.Product.update(updatedData, {
+      where: { id: req.params.id },
+    })
+      .then(() => {
+        res.redirect("/products/productCard");
+      })
+      .catch((err) => {
+        res.status(500).send({ error: err.message });
+      });
   },
   destroy: (req, res) => {
     db.Product.destroy({
--- src/services/productService.js
+++ src/services/productService.js
@@ -5,20 +5,6 @@ const productService = {
   getOne: function (id) {
     return this.getAll().find((product) => product.id == id);
   },
-  editProduct: function (id, updatedData) {
-    let products = this.getAll();
-    let index = products.findIndex((product) => product.id == id);
-    products[index] = { ...products[index], ...updatedData };
-    fs.writeFileSync(productsFilePath, JSON.stringify(products), "utf-8");
-  },
-  deleteProduct: function (id) {
-    let filteredElements = this.getAll().filter((product) => product.id != id);
-    fs.writeFileSync(
-      productsFilePath,
-      JSON.stringify(filteredElements),
-      "utf-8"
-    );
-  },
   save: function (product) {
     this.getAll().push(product);
     fs.writeFileSync(
