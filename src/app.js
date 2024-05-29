5823d1316b8f5f55c1ef8d3bf3d593e02c52aef0
DPalermo26 <palermodanilo01@gmail.com>
2024-05-29 11:19:25 -0300

    Fix config.js

--- src/app.js
+++ src/app.js
@@ -9,7 +9,6 @@ const cookieParser = require("cookie-parser");
 const recordameMiddleware = require("./middlewares/recordameMiddleware");
 
 const app = express();
-require("dotenv").config();
 
 app.use((req, res, next) => {
   console.log("Solicitud recibida:", req.method, req.url);
