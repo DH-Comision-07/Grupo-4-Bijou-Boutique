6110d8d4d65369de4441a2b883b6f497df12d6c1
Christofher15 <miladyabbara@gmail.com>
2024-06-05 13:02:46 -0300

    register

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,32 +1,30 @@
 function validateForm(event) {
-    event.preventDefault();
-}
+    event.preventDefault(); 
 
-const name = document.getElementById('name').value;
-const surname = document.getElementById('surname').value;
-const password = document.getElementById('pass').value;
-const email = document.getElementById('mail').value;
+    const name = document.getElementById('name').value;
+    const surname = document.getElementById('surname').value;
+    const password = document.getElementById('pass').value;
+    const email = document.getElementById('mail').value;
 
+    const nameSurnameRegex = /^.{3,}$/;
+    const passwordRegex = /^(?=.[0-9])(?=.[!@#$%^&])[A-Za-z\d!@#$%^&]{8,}$/;
 
-const nameSurnameRegex = /^.{3,}$/;
-const passwordRegex = /^(?=.[0-9])(?=.[!@#$%^&])[A-Za-z\d!@#$%^&]{8,}$/;
+    if (!nameSurnameRegex.test(name)) {
+        alert('El nombre debe tener al menos 3 caracteres.');
+        return false;
+    }
 
-if (!nameSurnameRegex.test(name)) {
-    alert('El nombre debe tener al menos 3 caracteres.');
-    return false;
-}
+    if (!nameSurnameRegex.test(surname)) {
+        alert('El apellido debe tener al menos 3 caracteres.');
+        return false;
+    }
 
-if (!nameSurnameRegex.test(surname)) {
-    alert('El apellido debe tener al menos 3 caracteres.');
-    return false;
-}
+    if (!passwordRegex.test(password)) {
+        alert('La contraseña debe tener al menos 8 caracteres, un número y un caracter especial.');
+        return false;
+    }
 
-if (!passwordRegex.test(password)) {
-    alert('La contraseña debe tener al menos 8 caracteres, un número y un caracter especial.');
-    return false;
-}
-
-event.target.submit();
+    event.target.submit();
 }
 
 document.addEventListener('DOMContentLoaded', () => {
