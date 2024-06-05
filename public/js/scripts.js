424497f91e9fb553b87c4675eeb6e354542f644a
Christofher15 <miladyabbara@gmail.com>
2024-06-05 12:55:31 -0300

    validciones

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -4,3 +4,5 @@ function validateForm(event) {
 
 const name = document.getElementById('name').value;
 const surname = document.getElementById('surname').value;
+const password = document.getElementById('pass').value;
+const email = document.getElementById('mail').value;
