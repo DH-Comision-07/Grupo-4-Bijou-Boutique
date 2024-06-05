839bdb4cd33f00f65cdeabb0f4e36e2540c6a205
Christofher15 <miladyabbara@gmail.com>
2024-06-05 12:58:50 -0300

    register

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -6,3 +6,8 @@ const name = document.getElementById('name').value;
 const surname = document.getElementById('surname').value;
 const password = document.getElementById('pass').value;
 const email = document.getElementById('mail').value;
+
+
+const nameSurnameRegex = /^.{3,}$/;
+    const passwordRegex = /^(?=.[0-9])(?=.[!@#$%^&])[A-Za-z\d!@#$%^&]{8,}$/;
+    
