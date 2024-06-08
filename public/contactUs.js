7694c1e2ea5eef22133087e6aebc891bb390275b
johannes003 <johannesandres03@gmail.com>
2024-06-08 11:06:33 -0500

    validacion

--- public/contactUs.js
+++ public/contactUs.js
@@ -4,4 +4,22 @@ document.addEventListener('DOMContentLoaded', function() {
     let email = document.getElementById('email').value;
     let mensaje = document.getElementById('mensaje').value;
 
-    
+    if (nombre.length < 3) {
+        alert('El nombre debe tener al menos 3 caracteres.');
+        event.preventDefault();  
+        return;
+    }
+
+    if (!email.includes('@')) {
+        alert('El email debe incluir un @.');
+        event.preventDefault();  
+        return;
+    }
+
+    if (mensaje.length < 10) {
+        alert('El mensaje debe tener al menos 10 caracteres.');
+        event.preventDefault();  
+        return;
+    }
+    });
+});
