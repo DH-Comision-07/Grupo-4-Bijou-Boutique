23621d8e048ee645b98c736b12179930a8f911cb
DPalermo26 <palermodanilo01@gmail.com>
2024-06-25 22:04:12 -0300

    Mejoras en vista de validaciones

--- src/middlewares/authMiddleware.js
+++ src/middlewares/authMiddleware.js
@@ -2,7 +2,7 @@ function authMiddleware(req, res, next) {
   if (req.session.usuarioLogueado != undefined) {
     next();
   } else {
-    res.send("Esta pagina es solo para usuarios!");
+    res.render("validation");
   }
 }
 
